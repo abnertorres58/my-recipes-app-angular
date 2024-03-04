941c29a0c8da3ed53fe02e557d5146647fff16f6
fred <abnertorres58@gmail.com>
2024-03-04 21:33:55 +0300

    Creating the first prototype of recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.eatingwell.com/thmb/fW8uYHYMRuY0v_hmZ3xyTtmb04o=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/EWL-255186-chickpea-curry-chhole-Step-01-B-fdcdbd14ce7f4eab901582eac22a8b27.jpg')
+  ];
 
 }
