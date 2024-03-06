ebd14dff360c06d4c8ee9320e7d348e88a063fa1
fred <abnertorres58@gmail.com>
2024-03-06 18:35:00 +0300

    Passing data using both Event and property binding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 import {Recipe} from "../recipe.model";
 
 @Component({
@@ -7,8 +7,14 @@ import {Recipe} from "../recipe.model";
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.eatingwell.com/thmb/fW8uYHYMRuY0v_hmZ3xyTtmb04o=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/EWL-255186-chickpea-curry-chhole-Step-01-B-fdcdbd14ce7f4eab901582eac22a8b27.jpg')
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.eatingwell.com/thmb/fW8uYHYMRuY0v_hmZ3xyTtmb04o=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/EWL-255186-chickpea-curry-chhole-Step-01-B-fdcdbd14ce7f4eab901582eac22a8b27.jpg'),
+    new Recipe('Another Test Recipe', 'This is simply a test', 'https://www.eatingwell.com/thmb/fW8uYHYMRuY0v_hmZ3xyTtmb04o=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/EWL-255186-chickpea-curry-chhole-Step-01-B-fdcdbd14ce7f4eab901582eac22a8b27.jpg')
   ];
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+
+  }
 }
